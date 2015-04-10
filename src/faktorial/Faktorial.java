2ab6b53b1ed5bf0cf2c62e7f500651ce8f1c1c9f
syifariaaa <syifariaaa@yahoo.com>
2015-04-10 10:51:58 +0700

    faktorial1

---
+++ src/faktorial/Faktorial.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package faktorial;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Syifa Ariandini
+ */
+public class Faktorial {
+    private int ang1;
+    public Faktorial(){
+        
+    }
+    
+    public static void main(String[] args) throws Exception{
+        Scanner input = new Scanner(System.in);
+        System.out.println("input angka :");
+        int inp = input.nextInt();
+        int b=1;
+        if(inp>0){
+            for(int i=1;i<inp;i++)
+            b=b*(inp-i);
+            b=b*inp;
+        }
+        else
+            b=1;
+        System.out.println("output : "+b);
+        
+        
+    }
+    
+}
